d5b15362a23daf821e8b0ab80bc923713b3809f1
anidas <89678814+anidas2002@users.noreply.github.com>
2021-10-20 12:40:11 +0400

    Update bb.js

--- bb.js
+++ bb.js
@@ -1,6 +1,13 @@
 
 		function sendEmail() {
 
+var name=document.getElementById('name').value;
+var email=document.getElementById('email').value;
+var mobile=document.getElementById('mobile').value;
+var company=document.getElementById('company').value;
+var subject=document.getElementById('subject').value;
+var message=document.getElementById('message').value;
+
 
 
 			Email.send({
@@ -12,13 +19,19 @@
 				To : 'anidas2002@gmail.com',
 				From : "westernshiporg@gmail.com",
 				Subject : "Website Enquiry",
-				Body : "New Enquiry"
+				Body : "New Enquiry"+"<br>"+"Name : "+name+"<br>"+"Email : "+email+"<br>"+"Mobile : "+mobile+"<br>"+"Company : "+company+
+"<br>"+"Subject : "+subject+"<br>"+"Message : "+message
 
 
 			})
 			.then(function(message){
 				alert("mail sent successfully")
-
+document.getElementById('name').value='';
+document.getElementById('email').value='';
+document.getElementById('mobile').value='';
+document.getElementById('company').value='';
+document.getElementById('subject').value='';
+document.getElementById('message').value='';
 
 			});
 		}
